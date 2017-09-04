c54745243c3a658c3770644ad81e60c89cfd68e9
yang <1301514511@qq.com>
2017-09-04 21:49:36 +0800

    dedecms Signed-off-by: yang <1301514511@qq.com>

---
+++ www.blog.com/data/common.inc.php
@@ -0,0 +1,12 @@
+<?php
+//数据库连接信息
+$cfg_dbtype = 'mysql';
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'blog_59';
+$cfg_dbuser = 'root';
+$cfg_dbpwd = 'root';
+$cfg_dbprefix = 'blog_';
+$cfg_db_language = 'utf8';
+
+
+?>
---
+++ www.blog.com/demo/js.php
@@ -0,0 +1,8 @@
+<?php
+
+$str = '<div class=\"nextinfo\">\
+        <p>上一篇：<a href=\"/a/manshenghuo/2017/0904/1.html\">某一人似曾相识、某一刻似曾经历</a> </p>\
+        <p>下一篇：没有了 </p>\
+      </div>';
+
+echo 'document.write("'.$str.'");';
